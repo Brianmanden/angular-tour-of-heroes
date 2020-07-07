d72a609436b87b6966fbf1abff78fd9173142215
Brianmanden <brianmanden@gmail.com>
2020-07-07 20:52:11 +0200

    Add selection list

---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr. Ice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeristas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMayn' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. LowIQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' },
+];
